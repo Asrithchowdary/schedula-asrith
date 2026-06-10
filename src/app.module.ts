0a3b4335818b3fa70608ddd48f47d62dc1983225
Asrithchowdary <127644464+Asrithchowdary@users.noreply.github.com>
2026-06-10 13:23:34 +0530

    configure neon database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,59 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { User } from './users/entities/user.entity';
 import { DoctorModule } from './doctor/doctor.module';
 import { PatientModule } from './patient/patient.module';
+
+import { User } from './users/entities/user.entity';
 import { DoctorProfile } from './doctor/doctor-profile.entity';
 import { PatientProfile } from './patient/patient-profile.entity';
 
+console.log(
+  'DATABASE_URL = ',
+  process.env.DATABASE_URL,
+);
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres123',
-      database: 'schedula_db',
-      entities: [User,DoctorProfile,PatientProfile,],
+
+      url: process.env.DATABASE_URL,
+
+      ssl: {
+        rejectUnauthorized: false,
+      },
+
+      autoLoadEntities: true,
+
+      entities: [
+        User,
+        DoctorProfile,
+        PatientProfile,
+      ],
+
       synchronize: true,
     }),
+
     AuthModule,
     UsersModule,
     DoctorModule,
     PatientModule,
   ],
+
   controllers: [AppController],
+
   providers: [AppService],
 })
 export class AppModule {}
